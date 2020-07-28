f53b35f51be9f537fb39f52b7499517dacc79873
sman01 <shreemun@gmail.com>
2020-07-28 18:37:08 +0530

    done with hangman project

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -7,43 +7,45 @@ g = 0
 se = set()
 i = 0
 s = ""
+ip = input('Type "play" to play the game, "exit" to quit:')
 op = list(sele)
 bs = [None] * len(op)
 tr = [None] * len(op)
-while i < 8:
-    for j in range(len(op)):
-        if op[j] == s and tr[j] is None:
-            bs[j] = s
-            tr[j] = 1
-        elif tr[j] is None:
-            bs[j] = "-"
+while ip == "play":
+    while i < 8:
+        for j in range(len(op)):
+            if op[j] == s and tr[j] is None:
+                bs[j] = s
+                tr[j] = 1
+            elif tr[j] is None:
+                bs[j] = "-"
 
-    if i < 8:
-        print()
-        print(''.join(bs))
-    s = input("Input a letter: ")
-    if len(s) > 1 or len(s) == 0:
-        print("You should input a single letter\n")
-        continue
-    elif ord(s) not in range(97, 123):
-        print("It is not an ASCII lowercase letter\n")
-        continue
-    elif s in se:
-        print("You already typed this letter\n")
-        continue
-    elif s not in sele:
-        i += 1
-        print("No such letter in the word")
+        if i < 8:
+            print()
+            print(''.join(bs))
+        s = input("Input a letter: ")
+        if len(s) > 1 or len(s) == 0:
+            print("You should input a single letter\n")
+            continue
+        elif ord(s) not in range(97, 123):
+            print("It is not an ASCII lowercase letter\n")
+            continue
+        elif s in se:
+            print("You already typed this letter\n")
+            continue
+        elif s not in sele:
+            i += 1
+            print("No such letter in the word")
 
-    if s not in se:
-        se.add(s)
+        if s not in se:
+            se.add(s)
 
-if "-" not in bs:
-    print("You guessed the word ".join(bs) + "!")
-    print("You survived!")
-else:
-    print("You are hanged!")
-
+    if "-" not in bs:
+        print("You guessed the word ".join(bs) + "!")
+        print("You survived!")
+    else:
+        print("You are hanged!")
+    ip = input('Type "play" to play the game, "exit" to quit:')
 
 
 
