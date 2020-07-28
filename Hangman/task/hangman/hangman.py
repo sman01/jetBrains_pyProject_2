a0fb13a8290873d661a0d1c52d9b880873279deb
sman01 <shreemun@gmail.com>
2020-07-28 18:33:58 +0530

    stage 7 done

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -5,22 +5,12 @@ selected = random.sample(choices, 1)
 sele = selected[0]
 g = 0
 se = set()
-b = "-" * len(sele)
-print(b)
 i = 0
+s = ""
 op = list(sele)
 bs = [None] * len(op)
 tr = [None] * len(op)
 while i < 8:
-    s = input("Input a letter:")
-    if s not in sele:
-        i += 1
-        print("No such letter in the word")
-    if s in se and s in sele:
-        i += 1
-        print("No improvements")
-    elif s not in se:
-        se.add(s)
     for j in range(len(op)):
         if op[j] == s and tr[j] is None:
             bs[j] = s
@@ -31,11 +21,27 @@ while i < 8:
     if i < 8:
         print()
         print(''.join(bs))
+    s = input("Input a letter: ")
+    if len(s) > 1 or len(s) == 0:
+        print("You should input a single letter\n")
+        continue
+    elif ord(s) not in range(97, 123):
+        print("It is not an ASCII lowercase letter\n")
+        continue
+    elif s in se:
+        print("You already typed this letter\n")
+        continue
+    elif s not in sele:
+        i += 1
+        print("No such letter in the word")
+
+    if s not in se:
+        se.add(s)
+
 if "-" not in bs:
-    print("You guessed the word")
+    print("You guessed the word ".join(bs) + "!")
     print("You survived!")
 else:
-
     print("You are hanged!")
 
 
---
+++ Problems/English contractions/task.py
@@ -0,0 +1,1 @@
+print("I am = I'm\nI have = I've\nI will = I'll\nI had / would = I'd")
